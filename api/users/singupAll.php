a1857a98c1a8309325b885bedc70bca58ac95193
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-03-28 05:09:59 -0600

    Fix - apis

--- api/users/singupAll.php
+++ api/users/singupAll.php
@@ -4,11 +4,12 @@ require "../config/database.php";
 class query extends Conexion {
     public function config() {
         $id = $_GET['id'];
+        $name = $_GET['name'];
         $password = $_GET['password'];
         try {
             $conexion = parent::conectar();
             $query = new MongoDB\Driver\BulkWrite;
-            $query->update(['_id' => new MongoDB\BSON\ObjectID($id)], ['$set' => ['password'=>$password]]);
+            $query->update(['_id' => new MongoDB\BSON\ObjectID($id)], ['$set' => ['name'=>$name, 'password'=>$password]]);
             $conexion->executeBulkWrite($this->database_name.$this->col_users, $query);
         } catch (\Throwable $th) {
             return $th->getMessage();
