ef76de2eb8cd466c04d1f9e9ccad85af845eb161
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-03-29 17:23:13 -0600

    Feat - client

--- api/config/database.php
+++ api/config/database.php
@@ -2,15 +2,16 @@
 class Conexion
 {
     public $conn;
-    public $database_name = "bordados.";
-    public $col_users = "usuarios";
+    public $database_name = "LosPajaritos.";
+    public $col_users = "users";
     public $col_categories = "categories";
     public $col_products = "products";
+    // $cadenaAnterior =  "mongodb+srv://user_all_privileges:pwd_mongodb@cluster0.vrmzrp9.mongodb.net/?retryWrites=true&w=majority";
 
     public function conectar()
     {
         try {
-            $cadenaConexion = "mongodb+srv://user_all_privileges:pwd_mongodb@cluster0.vrmzrp9.mongodb.net/?retryWrites=true&w=majority";
+            $cadenaConexion = "mongodb+srv://sastrerialospajaritos:fUIRMRzwPDAtkV9u@cluster43000.2ddxg0t.mongodb.net/?retryWrites=true&w=majority";
             $cliente = new MongoDB\Driver\Manager($cadenaConexion);
             return $cliente;
         } catch (\Throwable $th) {
